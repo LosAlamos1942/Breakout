79c5288c8da59aacab1e910fca8fc34830093283
Nerto Kageh <kagehnerto@yahoo.com>
2022-03-26 17:56:22 -0500

    Created ball object.

--- IOS App Breakout/IOS App Breakout/GameScene.swift
+++ IOS App Breakout/IOS App Breakout/GameScene.swift
@@ -9,12 +9,16 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    var ball = SKShapeNode()
     
     override func didMove(to view: SKView) {
-        func createBackground() {
-            
+        func resetGame() {
+                // this stuff happens before each game starts
+                makeBall()
+            }
+    }
+    func createBackground() {
             let stars = SKTexture(imageNamed: "Stars")
-            
             for i in 0...1 {
                 let starsBackground = SKSpriteNode(texture: stars)
                 starsBackground.zPosition = -1
@@ -25,9 +29,38 @@ class GameScene: SKScene {
                 let moveLoop = SKAction.sequence([moveDown, moveReset])
                 let moveForever = SKAction.repeatForever(moveLoop)
                 starsBackground.run(moveForever)
-                
             }
-            
         }
-    }
+
+    func makeBall() {
+            ball.removeFromParent()     // remove the ball (if it exists)
+            ball = SKShapeNode(circleOfRadius: 10)
+            ball.position = CGPoint(x: frame.midX, y: frame.midY)
+            ball.strokeColor = .black
+            ball.fillColor = .yellow
+            ball.name = "ball"
+
+            // physics shape matches ball image
+            ball.physicsBody = SKPhysicsBody(circleOfRadius: 10)
+            // ignores all forces and impulses
+            ball.physicsBody?.isDynamic = false
+            // use precise collision detection
+            ball.physicsBody?.usesPreciseCollisionDetection = true
+            // no loss of energy from friction
+            ball.physicsBody?.friction = 0
+            // gravity is not a factor
+            ball.physicsBody?.affectedByGravity = false
+            // bounces fully off of other objects
+            ball.physicsBody?.restitution = 1
+            // does not slow down over time
+            ball.physicsBody?.linearDamping = 0
+            ball.physicsBody?.contactTestBitMask = (ball.physicsBody?.collisionBitMask)!
+
+            addChild(ball)  // add ball object to the view
+        }
+
+
+
+
 }
+
