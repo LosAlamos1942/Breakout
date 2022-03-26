a2683a539acd850dd4989997fd40cdccf482ac85
Nerto Kageh <kagehnerto@yahoo.com>
2022-03-26 17:40:55 -0500

    Created background.

---
+++ IOS App Breakout/IOS App Breakout/GameScene.swift
@@ -0,0 +1,33 @@
+//
+//  GameScene.swift
+//  IOS App Breakout
+//
+//  Created by Student on 3/26/22.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        func createBackground() {
+            
+            let stars = SKTexture(imageNamed: "Stars")
+            
+            for i in 0...1 {
+                let starsBackground = SKSpriteNode(texture: stars)
+                starsBackground.zPosition = -1
+                starsBackground.position = CGPoint(x: 0, y: starsBackground.size.height * CGFloat(i))
+                addChild(starsBackground)
+                let moveDown = SKAction.moveBy(x: 0, y: -starsBackground.size.height, duration: 20)
+                let moveReset = SKAction.moveBy(x: 0, y: starsBackground.size.height, duration: 0)
+                let moveLoop = SKAction.sequence([moveDown, moveReset])
+                let moveForever = SKAction.repeatForever(moveLoop)
+                starsBackground.run(moveForever)
+                
+            }
+            
+        }
+    }
+}
